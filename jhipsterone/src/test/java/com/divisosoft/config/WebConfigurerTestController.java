2734b3061c38232705d64186f2066c88f1353f3e
Arjun7945 <arjunarjups111@gmail.com>
2025-01-20 18:16:21 +0530

    Created a crud operation using jhipster

---
+++ jhipsterone/src/test/java/com/divisosoft/config/WebConfigurerTestController.java
@@ -0,0 +1,18 @@
+package com.divisosoft.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebConfigurerTestController {
+
+    @GetMapping("/api/test-cors")
+    public void testCorsOnApiPath() {
+        // empty method
+    }
+
+    @GetMapping("/test/test-cors")
+    public void testCorsOnOtherPath() {
+        // empty method
+    }
+}
